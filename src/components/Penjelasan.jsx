2fadf53a89e441a7810596d0d4220e2560c23f68
AgungRezaV <agung.rezavergiawan.tik20@mhsw.pnj.ac.id>
2023-02-17 23:45:56 +0700

    Added new images from client

--- src/components/Penjelasan.jsx
+++ src/components/Penjelasan.jsx
@@ -1,6 +1,6 @@
 import { features } from '../constants';
 import styles, { layout } from '../style';
-import { doctor } from '../assets';
+import { doctor, sampul_web } from '../assets';
 
 const PenyebabCard = ({ icon, title, content, content2, content3, content4, i }) => (
     <div className={`flex flex-wrap: wrap rounded-[5px] sm:p-10 p-2 ${i !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card transition ease-in-out delay-150 hover:scale-110 hover:bg-blue-500 duration-500 ... `}>
@@ -29,18 +29,23 @@ const PenyebabCard = ({ icon, title, content, content2, content3, content4, i })
 const Penjelasan = () => {
     return (
         <section id='penjelasan' className="sm:py-16 py-6">
-            <div>
-                <h2 className="text-center font-poppins font-semibold xs:text-[56px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
-                    Stunting
-                    {/* <br className='sm:block hidden ' />
-                    Stunting disebabkan oleh faktor yang multidimensi, diantaranya: */}
-                </h2>
+            <div className='flex flex-col'>
+                <div className='flex md:flex-row flex-col'>
+                    <div className='flex flex-col'>
+                        <h2 className="font-poppins font-semibold xs:text-[56px] text-[40px] text-black w-full tracking-widest">
+                            Stunting
+                            {/* <br className='sm:block hidden ' />
+                                Stunting disebabkan oleh faktor yang multidimensi, diantaranya: */}
+                        </h2>
 
-                <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] w-full mt-5 text-black text-center">
-                    Atau biasa disebut kerdil atau pendek adalah kondisi gagal tumbuh oada anak berusia di bawah lima tahun (balita) akibat kekurangan gizi kronis
-                    dan infeksi berulang terutama pada periode 1.000 Hari Pertama Kehidupan (HPK), yaitu dari janin hingga anak berusia 23 bulan.
-                    Anak tergolong stunting apabila panjang atau tinggi badannya berada di bawah minus dua (-2) standar deviasi panjang atau tinggi badan anak seumurnya.
-                </p>
+                        <p className="font-poppins font-normal text-black text-[18px] w-full mt-5 text-black md:max-w-[700px] w-full text-justify px-12">
+                            Atau biasa disebut kerdil atau pendek adalah kondisi gagal tumbuh oada anak berusia di bawah lima tahun (balita) akibat kekurangan gizi kronis
+                            dan infeksi berulang terutama pada periode 1.000 Hari Pertama Kehidupan (HPK), yaitu dari janin hingga anak berusia 23 bulan.
+                            Anak tergolong stunting apabila panjang atau tinggi badannya berada di bawah minus dua (-2) standar deviasi panjang atau tinggi badan anak seumurnya.
+                        </p>
+                    </div>
+                    <img src={sampul_web} alt="sampul web" className='md:w-[50%] w-[100%] md:h-[100%] h-[50%] md:right-img center md:mt-0 mt-10 relative z-[5]' />
+                </div>
 
                 <div className='mt-10 content-align'>
                     <div className={`${layout.sectionImg} flex-wrap justify-center`}>
